08810c0342505cf5bde7adec110f41ba150c890c
hill2b7 <hill2b7@mail.uc.edu>
2023-02-01 17:05:42 -0500

    Task 1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        double priceOfPurchase = 10.99; // This sets the purchase price to an exact cost
+        double SALES_TAX_PERCENTAGE  = 0.05; //All capitals for a constant
+        double totalSalesTax = 0.00; // sets total sales tax
+        double totalPrice = 0.00; // sets total price
+
+        totalSalesTax = priceOfPurchase * SALES_TAX_PERCENTAGE; //calculation for total sales tax
+        System.out.println("The total sales tax is " + totalSalesTax); //outputs the calculation of sales tax
+        totalPrice = totalSalesTax + priceOfPurchase; // calculates total cost including sales tax and price of item
+        System.out.println("The total price is " + totalPrice); // outputs total price of sale
+    }
+
+}
